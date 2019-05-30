a7f25aac4c9431f88c474f689a638c746c702c57
Jean Pierobom <pierobom@gmail.com>
2019-05-30 13:02:20 -0700

    Adding an empty project

---
+++ jaguarteam-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  jaguarteam-ios
+//
+//  Created by Jean on 5/30/19.
+//  Copyright © 2019 Langara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
